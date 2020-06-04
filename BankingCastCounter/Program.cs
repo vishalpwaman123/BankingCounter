940b3dfca53e438c64145efcac64a7e422cfbddc
unknown <vishalpwaman123@gmail.com>
2020-06-03 23:54:04 -0700

    Welcome To Banking Cash Counter

---
+++ BankingCastCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingCastCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
